6545c39805e0216d93e8aad512b5d11bfe1e2ee7
Uchiha-Ichi <hoang290603@gmail.com>
2025-10-02 14:50:19 +0700

    login

--- apps/api/src/users/schemas/user.schema.ts
+++ apps/api/src/users/schemas/user.schema.ts
@@ -1,20 +1,21 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop({ required: true, unique: true })
+  @Prop({ required: false, unique: true, sparse: true })
   email: string;
 
-  @Prop({ required: true })
+  @Prop({ required: false })
   password: string; // Hashed password
 
   @Prop({ required: true })
   walletAddress?: string; // MetaMask wallet address
 
-  @Prop({ required: true, enum: ['admin', 'user'], default: 'user' })
+  @P
+  @Prop({ required: true, enum: ["admin", "user"], default: "user" })
   role: string;
 }
 
